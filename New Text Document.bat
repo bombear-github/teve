5b6fc1d11a23fbe67fedc8ef5468b858b7b6141b
bombear-github <khoala10@gmail.com>
2019-11-21 16:25:53 -0800

    Automatic Commit Message Goes Here

---
+++ New Text Document.bat
@@ -0,0 +1,3 @@
+git add 'second file.txt'
+git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`"
+git push -u origin master
---
+++ from powershell.bat
@@ -0,0 +1,1 @@
+& "C:\Program Files\Git\bin\bash.exe" -c "cd 'C:\Users\Khoa\Desktop\test\teve\' && git add --all  &&  git commit -m 'Automatic Commit Message Goes Here' && git push origin master"
